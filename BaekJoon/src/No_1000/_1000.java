724545e614488e933377371ea9b4cb892834ef53
Hoyoung Park <qkrghdud369@gmail.com>
2024-08-08 20:17:42 +0900

    test

---
+++ BaekJoon/src/No_1000/_1000.java
@@ -0,0 +1,22 @@
+package No_1000;
+
+import java.util.Scanner;
+
+public class _1000 {
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		
+		if(0 < A && B < 10) {
+			System.out.println((A + B));
+		}
+		else {
+			return;
+		}
+	}
+	
+
+}
