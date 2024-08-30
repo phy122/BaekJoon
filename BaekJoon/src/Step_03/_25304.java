ae2f1e1c69d5f607a161013ba1ff409ba1ef505e
Hoyoung Park <qkrghdud369@gmail.com>
2024-08-30 22:05:16 +0900

    11

--- BaekJoon/src/Step_03/_25304.java
+++ BaekJoon/src/Step_03/_25304.java
@@ -1,5 +1,15 @@
 package Step_03;
 
+import java.util.Scanner;
+
 public class _25304 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int X = sc.nextInt();
+		int N = sc.nextInt();
+		
+	}
+	
 
 }
